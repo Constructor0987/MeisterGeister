3578630bf2c656cda555922d09cadaa50e94a2d3
markus <markus@c5e1f230-26d4-41f2-a5c8-149495a0d85e>
2012-06-20 07:08:40 +0000

    Grundgerüst für VorNachteileViewModel
    git-svn-id: https://moonvega.pmhost.de/svn/meistergeister/branches/heldenmvvm@636 c5e1f230-26d4-41f2-a5c8-149495a0d85e

---
+++ ViewModel/Helden/Controls/VorNachteileViewModel.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MeisterGeister.ViewModel.Helden.Controls
+{
+    public class VorNachteileViewModel : Base.ViewModelBase
+    {
+        #region //---- FELDER ----
+
+        // Felder
+
+        // Listen
+
+        // Commands
+
+
+        #endregion
+
+        #region //---- EIGENSCHAFTEN ----
+
+        public Model.Held SelectedHeld
+        {
+            get { return Global.SelectedHeld; }
+            set
+            {
+                Global.SelectedHeld = value;
+                OnChanged("SelectedHeld");
+            }
+        }
+
+        // Listen
+
+
+        // Commands
+
+
+        #endregion
+
+        #region //---- KONSTRUKTOR ----
+
+        public VorNachteileViewModel()
+        {
+            // EventHandler für SelectedHeld registrieren
+            Global.HeldSelectionChanged += (s, ev) => { SelectedHeldChanged(); };
+        }
+
+        #endregion
+
+        #region //---- INSTANZMETHODEN ----
+
+        public void LoadDaten()
+        {
+                
+        }
+
+        #endregion
+
+        #region //---- EVENTS ----
+
+        private void SelectedHeldChanged()
+        {
+            OnChanged("SelectedHeld");
+        }
+
+        #endregion
+
+    }
+}
