b385a2c733b943814847c5ca1bef08661f4bdbd3
markus <markus@c5e1f230-26d4-41f2-a5c8-149495a0d85e>
2012-08-31 13:47:39 +0000

    Versionsprüfung für SQL-CE geändert, dass es auch mit einer neueren Version funktioniert
    git-svn-id: https://moonvega.pmhost.de/svn/meistergeister/branches/heldenmvvm@675 c5e1f230-26d4-41f2-a5c8-149495a0d85e

--- App.xaml.cs
+++ App.xaml.cs
@@ -80,8 +80,8 @@ namespace MeisterGeister
             // SQL-Server Compact Version prüfen
             object sqlCompactVersion = "-";
             sqlCompactVersion = Microsoft.Win32.Registry.GetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Microsoft SQL Server Compact Edition\v4.0", "Version", "-");
-            string sqlCompactVersionNeeded = "4.0.8482.1";
-            if (sqlCompactVersion == null || sqlCompactVersion.ToString() != sqlCompactVersionNeeded)
+            string sqlCompactVersionNeeded = "4.0.8"; //4.0.8482.1 oder 4.0.8876.1
+            if (sqlCompactVersion == null || !sqlCompactVersion.ToString().StartsWith(sqlCompactVersionNeeded))
             {
                 string msg = "Es ist keine, eine falsche oder eine beschädigte Microsoft SQL Server Compact Edition installiert.\n"
                     + "Installiert:\t" + sqlCompactVersion
