7465caa277229ecd7fb5d6cec0366e74ad8b3ae3
markus <markus@c5e1f230-26d4-41f2-a5c8-149495a0d85e>
2012-08-31 20:36:08 +0000

    Zauber-Tab in eigenen View ausgelagert; noch keine Funktionalität
    git-svn-id: https://moonvega.pmhost.de/svn/meistergeister/branches/heldenmvvm@680 c5e1f230-26d4-41f2-a5c8-149495a0d85e

---
+++ View/Helden/Controls/ZauberView.xaml.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MeisterGeister.View.Helden.Controls
+{
+    /// <summary>
+    /// Interaktionslogik für ZauberView.xaml
+    /// </summary>
+    public partial class ZauberView : UserControl
+    {
+        public ZauberView()
+        {
+            InitializeComponent();
+        }
+
+        //private DatabaseDSADataSet.Held_ZauberRow SelectedZauberRow
+        //{
+        //    get
+        //    {
+        //        DatabaseDSADataSet.Held_ZauberRow zauberRow = null;
+        //        if (_dataGridHeldZauber.SelectedItem != null)
+        //            zauberRow = (DatabaseDSADataSet.Held_ZauberRow)((System.Data.DataRowView)_dataGridHeldZauber.SelectedItem).Row;
+
+        //        return zauberRow;
+        //    }
+        //}
+
+        //private Model.Zauber SelectedZauber
+        //{
+        //    get { return new Zauber(SelectedZauberRow.ZauberRow); }
+        //}
+
+        private void ContextMenuZauber_Opened(object sender, RoutedEventArgs e)
+        {
+            if (_dataGridHeldZauber.SelectedItem == null)
+            {
+                _menuItemZauberLöschen.IsEnabled = false;
+                _menuItemZauberWiki.IsEnabled = false;
+            }
+            else
+            {
+                _menuItemZauberLöschen.IsEnabled = true;
+                _menuItemZauberWiki.IsEnabled = true;
+            }
+        }
+
+        private void MenuItemZauberProben_Click(object sender, RoutedEventArgs e)
+        {
+            // TODO MT: Probe würfeln
+            //if (ProbeWürfeln != null)
+            //{
+            //    ProbeWürfeln(SelectedZauberRow.ZauberRow.Name);
+            //}
+        }
+
+        private void _comboBoxZauber_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+                InsertZauber();
+        }
+
+        private void InsertZauber()
+        {
+            if (IsInitialized)
+            {
+                //if (_comboBoxZauber.SelectedItem != null && SelectedHeld.Id != Guid.Empty)
+                //{
+                //    var zauber = (System.Collections.Generic.KeyValuePair<string, int>)_comboBoxZauber.SelectedItem;
+                //    string rep = _comboBoxRepräsentation.SelectedValue.ToString();
+                //    if (SelectedHeld.AddZauber(zauber.Value, rep) == false)
+                //        MessageBox.Show(string.Format("Der Zauber '{0}' in der Repräsentation '{1}' ist bereits vorhanden.", zauber.Key, rep), "Zauber hinzufügen");
+                //    RefreshHeld(false);
+                //    SetZauberAktivierbar();
+                //}
+            }
+        }
+
+        private void SetZauberAktivierbar()
+        {
+            //_comboBoxZauber.ItemsSource = Zauber.ZauberList;
+            _comboBoxZauber.SelectedIndex = -1;
+        }
+
+        private void _comboBoxZauber_DropDownOpened(object sender, EventArgs e)
+        {
+            SetZauberAktivierbar();
+        }
+
+        private void _comboBoxZauber_DropDownClosed(object sender, EventArgs e)
+        {
+            InsertZauber();
+        }
+
+        private void _dataGridHeldZauber_PreviewKeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if (sender is System.Windows.Controls.DataGrid && e.Key == Key.Delete)
+            {
+                var grid = (System.Windows.Controls.DataGrid)sender;
+
+                if (grid.SelectedItems.Count > 0)
+                {
+                    //DatabaseDSADataSet.Held_ZauberRow zauber = SelectedZauberRow;
+                    //if (MessageBox.Show(string.Format("Soll der Zauber '{0}' entfernt werden?", zauber.ZauberRow.Name), "Zauber entfernen", MessageBoxButton.YesNo,
+                    //                    MessageBoxImage.Question, MessageBoxResult.Yes) != MessageBoxResult.Yes)
+                    //{
+                    //    e.Handled = true;
+                    //}
+                }
+            }
+        }
+
+        private void MenuItemZauberLöschen_Click(object sender, RoutedEventArgs e)
+        {
+            DeleteHeldZauber();
+        }
+
+        private void DeleteHeldZauber()
+        {
+            //DatabaseDSADataSet.Held_ZauberRow zauber = SelectedZauberRow;
+            //if (MessageBox.Show(string.Format("Soll der Zauber '{0}' entfernt werden?", zauber.ZauberRow.Name), "Zauber entfernen", MessageBoxButton.YesNo,
+            //                            MessageBoxImage.Question, MessageBoxResult.Yes) == MessageBoxResult.Yes)
+            //{
+            //    SelectedHeld.DeleteZauber(zauber);
+            //}
+        }
+
+        private void MenuItemZauberWiki_Click(object sender, RoutedEventArgs e)
+        {
+            //System.Diagnostics.Process.Start("http://www.wiki-aventurica.de/wiki/" + SelectedZauber.WikiLink);
+        }  
+    }
+}
--- View/Helden/HeldenView.xaml.cs
+++ View/Helden/HeldenView.xaml.cs
@@ -30,7 +30,6 @@ namespace MeisterGeister.View.Helden
 
 
             _comboBoxSonderfertigkeit.SelectedIndex = -1;
-            _comboBoxZauber.SelectedIndex = -1;
         }
 
         public event System.ComponentModel.PropertyChangedEventHandler HeldChanged;
@@ -71,21 +70,22 @@ namespace MeisterGeister.View.Helden
             }
             else
             {
-                if (refreshRepräsentationen)
-                    _comboBoxRepräsentation.SelectedValue = held.RepräsentationStandard();
+                // TODO MT: In ZauberView integrieren
+                //if (refreshRepräsentationen)
+                //    _comboBoxRepräsentation.SelectedValue = held.RepräsentationStandard();
 
-                // Zauber-Sortierung aktualisieren
-                if (_dataGridHeldZauber.Items is System.Windows.Data.CollectionView)
-                {
-                    System.Windows.Data.CollectionViewSource csv = (System.Windows.Data.CollectionViewSource)FindResource("heldHeld_ZauberViewSource");
-                    if (csv != null && csv.View != null)
-                    {
-                        csv.View.SortDescriptions.Clear();
-                        csv.View.SortDescriptions.Add(new System.ComponentModel.SortDescription("Zaubername", System.ComponentModel.ListSortDirection.Ascending));
-                        csv.View.SortDescriptions.Add(new System.ComponentModel.SortDescription("Repräsentation", System.ComponentModel.ListSortDirection.Ascending));
-                        _dataGridHeldZauber.ItemsSource = csv.View;
-                    }
-                }
+                //// Zauber-Sortierung aktualisieren
+                //if (_dataGridHeldZauber.Items is System.Windows.Data.CollectionView)
+                //{
+                //    System.Windows.Data.CollectionViewSource csv = (System.Windows.Data.CollectionViewSource)FindResource("heldHeld_ZauberViewSource");
+                //    if (csv != null && csv.View != null)
+                //    {
+                //        csv.View.SortDescriptions.Clear();
+                //        csv.View.SortDescriptions.Add(new System.ComponentModel.SortDescription("Zaubername", System.ComponentModel.ListSortDirection.Ascending));
+                //        csv.View.SortDescriptions.Add(new System.ComponentModel.SortDescription("Repräsentation", System.ComponentModel.ListSortDirection.Ascending));
+                //        _dataGridHeldZauber.ItemsSource = csv.View;
+                //    }
+                //}
             }
             // Geweiht und Karmaenergie
             if (!held.Geweiht)
@@ -161,24 +161,6 @@ namespace MeisterGeister.View.Helden
             DeleteHeldSonderfertigkeit();
         }
 
-
-        private DatabaseDSADataSet.Held_ZauberRow SelectedZauberRow
-        {
-            get
-            {
-                DatabaseDSADataSet.Held_ZauberRow zauberRow = null;
-                if (_dataGridHeldZauber.SelectedItem != null)
-                    zauberRow = (DatabaseDSADataSet.Held_ZauberRow)((System.Data.DataRowView)_dataGridHeldZauber.SelectedItem).Row;
-
-                return zauberRow;
-            }
-        }
-
-        private Zauber SelectedZauber
-        {
-            get { return new Zauber(SelectedZauberRow.ZauberRow); }
-        }
-
         private void DeleteHeldSonderfertigkeit()
         {
             DatabaseDSADataSet.Held_SonderfertigkeitRow sf = SelectedSonderfertigkeit;
@@ -192,37 +174,8 @@ namespace MeisterGeister.View.Helden
             }
         }
 
-     
-
-        private void ContextMenuZauber_Opened(object sender, RoutedEventArgs e)
-        {
-            if (_dataGridHeldZauber.SelectedItem == null)
-            {
-                _menuItemZauberLöschen.IsEnabled = false;
-                _menuItemZauberWiki.IsEnabled = false;
-            }
-            else
-            {
-                _menuItemZauberLöschen.IsEnabled = true;
-                _menuItemZauberWiki.IsEnabled = true;
-            }
-        }
-
-       
-
-        
-
         public event ProbeWürfelnEventHandler ProbeWürfeln;
 
-
-
-        private void MenuItemZauberProben_Click(object sender, RoutedEventArgs e)
-        {
-            if (ProbeWürfeln != null)
-            {
-                ProbeWürfeln(SelectedZauberRow.ZauberRow.Name);
-            }
-        }
 
         private void MenuItemSonderfertigkeitWiki_Click(object sender, RoutedEventArgs e)
         {
@@ -250,10 +203,6 @@ namespace MeisterGeister.View.Helden
             }
         }
                
-        
-        
-
-
         private void _comboBoxSonderfertigkeit_DropDownOpened(object sender, EventArgs e)
         {
             SetSonderfertigkeitenAktivierbar();
@@ -291,84 +240,6 @@ namespace MeisterGeister.View.Helden
                 }
             }
         }
-
-        private void _comboBoxZauber_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
-        {
-            if (e.Key == Key.Enter)
-                InsertZauber();
-        }
-
-        private void InsertZauber()
-        {
-            if (IsInitialized)
-            {
-                if (_comboBoxZauber.SelectedItem != null && SelectedHeld.Id != Guid.Empty)
-                {
-                    var zauber = (System.Collections.Generic.KeyValuePair<string, int>)_comboBoxZauber.SelectedItem;
-                    string rep = _comboBoxRepräsentation.SelectedValue.ToString();
-                    if (SelectedHeld.AddZauber(zauber.Value, rep) == false)
-                        MessageBox.Show(string.Format("Der Zauber '{0}' in der Repräsentation '{1}' ist bereits vorhanden.", zauber.Key, rep), "Zauber hinzufügen");
-                    RefreshHeld(false);
-                    SetZauberAktivierbar();
-                }
-            }
-        }
-
-        private void SetZauberAktivierbar()
-        {
-            _comboBoxZauber.ItemsSource = Zauber.ZauberList;
-            _comboBoxZauber.SelectedIndex = -1;
-        }
-
-        private void _comboBoxZauber_DropDownOpened(object sender, EventArgs e)
-        {
-            SetZauberAktivierbar();
-        }
-
-        private void _comboBoxZauber_DropDownClosed(object sender, EventArgs e)
-        {
-            InsertZauber();
-        }
-
-        private void _dataGridHeldZauber_PreviewKeyDown(object sender, System.Windows.Input.KeyEventArgs e)
-        {
-            if (sender is System.Windows.Controls.DataGrid && e.Key == Key.Delete)
-            {
-                var grid = (System.Windows.Controls.DataGrid)sender;
-
-                if (grid.SelectedItems.Count > 0)
-                {
-                    DatabaseDSADataSet.Held_ZauberRow zauber = SelectedZauberRow;
-                    if (MessageBox.Show(string.Format("Soll der Zauber '{0}' entfernt werden?", zauber.ZauberRow.Name), "Zauber entfernen", MessageBoxButton.YesNo,
-                                        MessageBoxImage.Question, MessageBoxResult.Yes) != MessageBoxResult.Yes)
-                    {
-                        e.Handled = true;
-                    }
-                }
-            }
-        }
-
-        private void MenuItemZauberLöschen_Click(object sender, RoutedEventArgs e)
-        {
-            DeleteHeldZauber();
-        }
-
-        private void DeleteHeldZauber()
-        {
-            DatabaseDSADataSet.Held_ZauberRow zauber = SelectedZauberRow;
-            if (MessageBox.Show(string.Format("Soll der Zauber '{0}' entfernt werden?", zauber.ZauberRow.Name), "Zauber entfernen", MessageBoxButton.YesNo,
-                                        MessageBoxImage.Question, MessageBoxResult.Yes) == MessageBoxResult.Yes)
-            {
-                SelectedHeld.DeleteZauber(zauber);
-            }
-        }
-
-        private void MenuItemZauberWiki_Click(object sender, RoutedEventArgs e)
-        {
-            System.Diagnostics.Process.Start("http://www.wiki-aventurica.de/wiki/" + SelectedZauber.WikiLink);
-        }                
-
-        
         
     }
 }
