bef1485bef3102988e35c40e99e14e7f6e8d1d13
markus <markus@c5e1f230-26d4-41f2-a5c8-149495a0d85e>
2012-10-17 09:56:21 +0000

    Proben-Tool: - weiteres Layout - Filter-ComboBox (Proben-Liste wird noch nicht gefiltert!!!)
    git-svn-id: https://moonvega.pmhost.de/svn/meistergeister/trunk@892 c5e1f230-26d4-41f2-a5c8-149495a0d85e

--- ViewModel/Proben/ProbenViewModel.cs
+++ ViewModel/Proben/ProbenViewModel.cs
@@ -42,7 +42,7 @@ namespace MeisterGeister.ViewModel.Proben
             }
         }
 
-        Probe _selectedProbe = null;
+        private Probe _selectedProbe = null;
         public Probe SelectedProbe
         {
             get { return _selectedProbe; }
@@ -51,6 +51,17 @@ namespace MeisterGeister.ViewModel.Proben
                 _selectedProbe = value;
                 RefreshProbeErgebnisListe();
                 OnChanged("SelectedProbe");
+            }
+        }
+
+        private FilterItem _selectedFilterItem = null;
+        public FilterItem SelectedFilterItem
+        {
+            get { return _selectedFilterItem; }
+            set 
+            { 
+                _selectedFilterItem = value;
+                OnChanged("SelectedFilterItem");
             }
         }
 
@@ -104,6 +115,13 @@ namespace MeisterGeister.ViewModel.Proben
             set { _probeListe = value; OnChanged("ProbeListe"); }
         }
 
+        List<FilterItem> _filterListe = new List<FilterItem>();
+        public List<FilterItem> FilterListe
+        {
+            get { return _filterListe; }
+            set { _filterListe = value; OnChanged("FilterListe"); }
+        }
+
         #endregion
 
         #region //---- KONSTRUKTOR ----
@@ -113,6 +131,29 @@ namespace MeisterGeister.ViewModel.Proben
             onWürfeln = new Base.CommandBase(Würfeln, null);
             Einstellungen.WuerfelSoundAbspielenChanged += WuerfelSoundAbspielenChanged;
             Global.GruppenProbeWürfeln += Global_GruppenProbeWürfeln;
+
+            // Filter-Liste
+            _filterListe.Add(new FilterItem("Alle"));
+            _filterListe.Add(new FilterItem("Häufig verwendet"));
+            _filterListe.Add(new FilterItem("Eigenschaften"));
+
+            _filterListe.Add(new FilterItem("Talente"));
+            // Talentgruppen
+            _filterListe.Add(new FilterItem("Kampf"));
+            _filterListe.Add(new FilterItem("Körper"));
+            _filterListe.Add(new FilterItem("Gesellschaft"));
+            _filterListe.Add(new FilterItem("Natur"));
+            _filterListe.Add(new FilterItem("Wissen"));
+            _filterListe.Add(new FilterItem("Handwerk"));
+            _filterListe.Add(new FilterItem("Sprachen/Schriften"));
+            _filterListe.Add(new FilterItem("Gaben"));
+            _filterListe.Add(new FilterItem("Ritualkenntnisse"));
+            _filterListe.Add(new FilterItem("Liturgiekenntnisse"));
+            _filterListe.Add(new FilterItem("Meta"));
+            _filterListe.Add(new FilterItem("Basis"));
+            _filterListe.Add(new FilterItem("Spezial"));
+
+            _filterListe.Add(new FilterItem("Zauber"));
 
             Refresh();
             RefreshProbeErgebnisListe();
@@ -211,6 +252,94 @@ namespace MeisterGeister.ViewModel.Proben
     }
 
     #region //---- SUBKLASSEN ----
+
+    public class FilterItem
+    {
+        public FilterItem(string name)
+        {
+            Name = name;
+            SetImagePath();
+        }
+        public string Name { get; set; }
+
+        private string _imagePath = string.Empty;
+        public string ImagePath
+        {
+            get { return _imagePath; }
+            set { _imagePath = value; }
+        }
+
+        private void SetImagePath()
+        {
+            switch (Name)
+            {
+                case "Alle":
+                    // TODO ??: Passenderes Icon
+                    _imagePath = "/DSA MeisterGeister;component/Images/Icons/General/filter.png";
+                    break;
+                case "Häufig verwendet":
+                    _imagePath = "/DSA MeisterGeister;component/Images/Icons/General/neu.png";
+                    break;
+                case "Eigenschaften":
+                    // TODO ??: Passenderes Icon
+                    _imagePath = "/DSA MeisterGeister;component/Images/Icons/Wuerfel/w20.png";
+                    break;
+                case "Talente":
+                    // TODO ??: Passenderes Icon
+                    _imagePath = "/DSA MeisterGeister;component/Images/Icons/helden.png";
+                    break;
+                case "Kampf":
+                    _imagePath = "/DSA MeisterGeister;component/Images/Icons/nahkampf_01.png";
+                    break;
+                case "Körper":
+                    _imagePath = "/DSA MeisterGeister;component/Images/Icons/ueberanstrengung.png";
+                    break;
+                case "Gesellschaft":
+                    _imagePath = "/DSA MeisterGeister;component/Images/Icons/helden_kopieren.png";
+                    break;
+                case "Natur":
+                    _imagePath = "/DSA MeisterGeister;component/Images/Icons/kraeutersuche.png";
+                    break;
+                case "Wissen":
+                    _imagePath = "/DSA MeisterGeister;component/Images/Icons/foliant.png";
+                    break;
+                case "Handwerk":
+                    _imagePath = "/DSA MeisterGeister;component/Images/Icons/schmiede.png";
+                    break;
+                case "Sprachen/Schriften":
+                    _imagePath = "/DSA MeisterGeister;component/Images/Icons/sprache.png";
+                    break;
+                case "Gaben":
+                    _imagePath = "/DSA MeisterGeister;component/Images/Icons/hesinde.png";
+                    break;
+                case "Ritualkenntnisse":
+                    _imagePath = "/DSA MeisterGeister;component/Images/Icons/zauberzeichen.png";
+                    break;
+                case "Liturgiekenntnisse":
+                    // TODO ??: Passenderes Icon
+                    _imagePath = "/DSA MeisterGeister;component/Images/Icons/audio2.png";
+                    break;
+                case "Meta":
+                    _imagePath = "/DSA MeisterGeister;component/Images/Icons/jagd.png";
+                    break;
+                case "Basis":
+                    // TODO ??: Passenderes Icon
+                    _imagePath = "/DSA MeisterGeister;component/Images/Icons/General/kreise.png";
+                    break;
+                case "Spezial":
+                    // TODO ??: Passenderes Icon
+                    _imagePath = "/DSA MeisterGeister;component/Images/Icons/General/info.png";
+                    break;
+                case "Zauber":
+                    _imagePath = "/DSA MeisterGeister;component/Images/Icons/magie.png";
+                    break;
+                default:
+                    // TODO ??: Passenderes Icon
+                    _imagePath = "/DSA MeisterGeister;component/Images/Icons/General/question.png";
+                    break;
+            }
+        }
+    }
 
     public class ProbeErgebnisItem : Base.ViewModelBase
     {
