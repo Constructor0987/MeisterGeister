1c164fe68013494f55b9d7d872d7eb238df47653
jbos <jbos@c5e1f230-26d4-41f2-a5c8-149495a0d85e>
2012-10-18 18:50:20 +0000

    Audioshuffle Audio-Begin/End
    git-svn-id: https://moonvega.pmhost.de/svn/meistergeister/trunk@912 c5e1f230-26d4-41f2-a5c8-149495a0d85e

---
+++ View/AudioPlayer/SliderRange.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MeisterGeister.View.AudioPlayer
+{
+    /// <summary>
+    /// Interaktionslogik für SliderRange.xaml
+    /// </summary>
+    public partial class SliderRange : UserControl
+    {
+        public SliderRange()
+        {
+            InitializeComponent();
+
+            this.Loaded += Slider_Loaded;
+        }
+
+        void Slider_Loaded(object sender, RoutedEventArgs e)
+        {
+            LowerSlider.ValueChanged += LowerSlider_ValueChanged;
+            UpperSlider.ValueChanged += UpperSlider_ValueChanged;
+        }
+
+        private void LowerSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            UpperSlider.Value = Math.Max(UpperSlider.Value, LowerSlider.Value);
+            UpperValue = UpperSlider.Value;
+            brdLine.Margin = new Thickness((this.Width / Maximum) * LowerValue, 0, this.Width - (this.Width / Maximum) * UpperValue, 0);
+        }
+
+        private void UpperSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            LowerSlider.Value = Math.Min(UpperSlider.Value, LowerSlider.Value);
+            LowerValue = LowerSlider.Value;
+            brdLine.Margin = new Thickness((this.Width / Maximum) * LowerValue, 0, this.Width - (this.Width / Maximum) * UpperValue, 0);
+        }
+
+        public double Minimum
+        {
+            get { return (double)GetValue(MinimumProperty); }
+            set { SetValue(MinimumProperty, value); }
+        }
+
+
+        public static readonly DependencyProperty MinimumProperty =
+            DependencyProperty.Register("Minimum", typeof(double), typeof(SliderRange), new UIPropertyMetadata(0d));
+
+        public double LowerValue
+        {
+            get { return (double)GetValue(LowerValueProperty); }
+            set { SetValue(LowerValueProperty, value); }
+        }
+
+        public static readonly DependencyProperty LowerValueProperty =
+            DependencyProperty.Register("LowerValue", typeof(double), typeof(SliderRange), new UIPropertyMetadata(0d));
+
+        public double UpperValue
+        {
+            get { return (double)GetValue(UpperValueProperty); }
+            set { SetValue(UpperValueProperty, value); }
+        }
+
+        public static readonly DependencyProperty UpperValueProperty =
+            DependencyProperty.Register("UpperValue", typeof(double), typeof(SliderRange), new UIPropertyMetadata(0d));
+
+        public double Maximum
+        {
+            get { return (double)GetValue(MaximumProperty); }
+            set { SetValue(MaximumProperty, value); }
+        }
+
+        public static readonly DependencyProperty MaximumProperty =
+            DependencyProperty.Register("Maximum", typeof(double), typeof(SliderRange), new UIPropertyMetadata(1d));
+
+    }
+}
