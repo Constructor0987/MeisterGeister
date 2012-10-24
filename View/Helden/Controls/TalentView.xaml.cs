def78ab46ce7c09ab38b848ca9ea1cc0146ac0e5
markus <markus@c5e1f230-26d4-41f2-a5c8-149495a0d85e>
2012-10-24 07:40:32 +0000

    einige Kleinigkeiten im Talent-Tab
    git-svn-id: https://moonvega.pmhost.de/svn/meistergeister/trunk@949 c5e1f230-26d4-41f2-a5c8-149495a0d85e

--- View/Helden/Controls/TalentView.xaml.cs
+++ View/Helden/Controls/TalentView.xaml.cs
@@ -21,13 +21,17 @@ namespace MeisterGeister.View.Helden.Controls {
     /// </summary>
     public partial class TalentView : UserControl {
 
-        #region//KONSTRUKTOR
+        #region // ---- KONSTRUKTOR ----
+
         public TalentView() {
             this.InitializeComponent();
             VM = new VM.TalentViewModel(View.General.ViewHelper.Popup, View.General.ViewHelper.Confirm, View.General.ViewHelper.ShowProbeDialog, View.General.ViewHelper.ShowError);
         }
+
         #endregion
-        #region
+
+        #region // ---- EIGENSCHAFTEN ----
+
         /// <summary>
         /// Ruft das ViewModel des Views ab oder legt es fest und weist das ViewModel dem DataContext zu.
         /// </summary>
@@ -41,6 +45,10 @@ namespace MeisterGeister.View.Helden.Controls {
             }
             set { DataContext = value; }
         }
+
+        #endregion
+
+        #region // ---- EVENTS ----
 
         private void UserControl_Loaded(object sender, RoutedEventArgs e) {
             try {
@@ -67,9 +75,23 @@ namespace MeisterGeister.View.Helden.Controls {
             //    _menuItemTalentProben.IsEnabled = true;
             //}
         }
+        
+        private void brdKlicked(object sender, System.Windows.Input.MouseButtonEventArgs e) 
+        {
+            int idxTalent = ((sender as Border).Parent as StackPanel).Children.IndexOf(sender as Border);
+            CloseAllExpanderTalente();
+            (spHeldTalent.Children[idxTalent] as Expander).IsExpanded = true;
+        }
 
-        #endregion
-        #region Methoden
+        private void DataGrid_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            scrollViewer.ScrollToVerticalOffset(scrollViewer.VerticalOffset - e.Delta);
+        }
+
+        #endregion // ---- EVENTS ----
+
+        #region // ---- METHODEN ----
+
         private void CloseAllExpanderTalente() {
             expGesellschaft.IsExpanded = false;
             expHandwerk.IsExpanded = false;
@@ -82,13 +104,7 @@ namespace MeisterGeister.View.Helden.Controls {
             expLiturgie.IsExpanded = false;
             expGaben.IsExpanded = false;
         }
-        #endregion
-        #region UI
-        private void brdKlicked(object sender, System.Windows.Input.MouseButtonEventArgs e) {
-            int idxTalent = ((sender as Border).Parent as StackPanel).Children.IndexOf(sender as Border);
-            CloseAllExpanderTalente();
-            (spHeldTalent.Children[idxTalent] as Expander).IsExpanded = true;
-        }
+
         #endregion
 
     }
