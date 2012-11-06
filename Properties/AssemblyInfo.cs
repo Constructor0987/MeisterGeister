266771b9b9a4aa3e6b94b969f4b463e29e1a9ff9
markus <markus@c5e1f230-26d4-41f2-a5c8-149495a0d85e>
2012-11-06 10:05:29 +0000

    - Fenster-Titel bei Arena geändert - Versions-Nr++
    git-svn-id: https://moonvega.pmhost.de/svn/meistergeister/trunk@1037 c5e1f230-26d4-41f2-a5c8-149495a0d85e

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -51,8 +51,8 @@ using System.Windows;
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.5.9.1")]
-[assembly: AssemblyFileVersion("1.5.9.1")]
+[assembly: AssemblyVersion("1.5.10.1")]
+[assembly: AssemblyFileVersion("1.5.10.1")]
 
 // Dem Testprojekt den Zugriff auf die Interna erlauben.
 [assembly: InternalsVisibleTo("MeisterGeister_Tests")]
