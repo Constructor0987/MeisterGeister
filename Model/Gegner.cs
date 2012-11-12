65c84e9b427d8329c579933b2f1784fc0cd1e853
markus <markus@c5e1f230-26d4-41f2-a5c8-149495a0d85e>
2012-11-12 13:12:26 +0000

    TP-Anzeige bei Gegner-Angriff korrigiert
    git-svn-id: https://moonvega.pmhost.de/svn/meistergeister/trunk@1100 c5e1f230-26d4-41f2-a5c8-149495a0d85e

--- Model/Gegner.cs
+++ Model/Gegner.cs
@@ -12,7 +12,7 @@ using System.ComponentModel;
 
 namespace MeisterGeister.Model
 {
-    public partial class Gegner : KampfLogic.Wesen, KampfLogic.IKämpfer, KampfLogic.IGegnerBase, Extensions.IInitializable, KampfLogic.IHasZonenRs
+    public partial class Gegner : KampfLogic.Wesen, KampfLogic.IKämpfer, KampfLogic.IGegnerBase, Extensions.IInitializable, KampfLogic.IHasZonenRs, IHasWunden
     {
         public Gegner() : base()
         {
