5e16fee38c01e2261ab616734b0c10c83649fb47
jonas <jonas@c5e1f230-26d4-41f2-a5c8-149495a0d85e>
2012-11-12 19:22:04 +0000

    Testversion 1.5.1.12
    git-svn-id: https://moonvega.pmhost.de/svn/meistergeister/trunk@1106 c5e1f230-26d4-41f2-a5c8-149495a0d85e

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -51,8 +51,8 @@ using System.Windows;
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.5.11.1")]
-[assembly: AssemblyFileVersion("1.5.11.1")]
+[assembly: AssemblyVersion("1.5.12.1")]
+[assembly: AssemblyFileVersion("1.5.12.1")]
 
 // Dem Testprojekt den Zugriff auf die Interna erlauben.
 [assembly: InternalsVisibleTo("MeisterGeister_Tests")]
