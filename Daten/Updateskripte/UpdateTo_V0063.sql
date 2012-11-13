890b8773a33a04517a4ac53df8618796801b4fc3
jonas <jonas@c5e1f230-26d4-41f2-a5c8-149495a0d85e>
2012-11-13 18:08:52 +0000

    Default-Setting und Tulamidenlande (Kultur)
    git-svn-id: https://moonvega.pmhost.de/svn/meistergeister/trunk@1121 c5e1f230-26d4-41f2-a5c8-149495a0d85e

--- Daten/Updateskripte/UpdateTo_V0063.sql
+++ Daten/Updateskripte/UpdateTo_V0063.sql
@@ -1,2 +1,5 @@
 ﻿/* Erweiterung der Setting-Tabelle */
 ALTER TABLE Setting ADD Aktiv bit not null default 0;
+UPDATE Setting SET Aktiv=1 WHERE SettingGUID='00000000-0000-0000-5e77-000000000001';
+UPDATE Kultur set Setting='Aventurien' WHERE Setting is NULL;
+UPDATE Kultur SET Name='Tulamidenlande',Variante='Tulamidenlande'  WHERE KulturGUID='00000000-0000-0000-0000-000000000280';
