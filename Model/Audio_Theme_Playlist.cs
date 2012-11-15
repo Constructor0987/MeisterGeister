2a97f55b2f430f5f1bb323e2272619fdc049fc10
jbos <jbos@c5e1f230-26d4-41f2-a5c8-149495a0d85e>
2012-11-15 17:17:06 +0000

    Audio_Theme_Playlist für AudioThemes
    git-svn-id: https://moonvega.pmhost.de/svn/meistergeister/trunk@1159 c5e1f230-26d4-41f2-a5c8-149495a0d85e

---
+++ Model/Audio_Theme_Playlist.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MeisterGeister.Model
+{
+    public partial class Audio_Theme_Playlist
+    {
+        public Audio_Theme_Playlist()
+        {
+           // Audio_PlaylistGUID = Guid.NewGuid();
+        }
+    }
+}
