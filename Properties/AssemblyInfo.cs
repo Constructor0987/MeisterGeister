690669aad50dc04460bc090e77843f2021d668ff
markus <markus@c5e1f230-26d4-41f2-a5c8-149495a0d85e>
2012-11-20 07:27:27 +0000

    Versionsnummer für Release
    git-svn-id: https://moonvega.pmhost.de/svn/meistergeister/trunk@1179 c5e1f230-26d4-41f2-a5c8-149495a0d85e

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -51,8 +51,8 @@ using System.Windows;
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.0.3")]
-[assembly: AssemblyFileVersion("2.0.0.3")]
+[assembly: AssemblyVersion("2.0.0.1")]
+[assembly: AssemblyFileVersion("2.0.0.1")]
 
 // Dem Testprojekt den Zugriff auf die Interna erlauben.
 [assembly: InternalsVisibleTo("MeisterGeister_Tests")]
