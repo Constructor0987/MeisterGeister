b26a06b4e9f9b911a029aebcb83d8e38ee833011
markus <markus@c5e1f230-26d4-41f2-a5c8-149495a0d85e>
2013-01-22 10:01:45 +0000

    Neue Zauber aus "Stätten okkulter Macht" - Beispiel Held aus der Helden-Software - Notizen in Skript angelegt
    --> Ist noch nicht umgesetzt! Nur vorbereitet, damit es nicht vergessen wird!
    
    git-svn-id: https://moonvega.pmhost.de/svn/meistergeister/trunk@1241 c5e1f230-26d4-41f2-a5c8-149495a0d85e

---
+++ Daten/Updateskripte/UpdateTo_V0070.sql
@@ -0,0 +1,7 @@
+﻿-- Neue Zauber aus Stätten okkulter Macht
+--Zauber: Ecliptifactus Schattenkraft (obsk.) [Mag]
+--Zauber: Nuntiovolo Botenvogel (obsk.) [Mag]
+--Zauber: Opacitas Schattenleib [Mag]
+--Zauber: Pantenebrum [Mag]
+--Zauber: Schattenodem [Mag]
+--Zauber: Umbraporta Schattentüre [Mag]
