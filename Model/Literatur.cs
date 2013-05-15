972dcd4139a430180fb89fd0954c220f1813f0f6
jonas <jonas@c5e1f230-26d4-41f2-a5c8-149495a0d85e>
2013-05-15 13:50:53 +0000

    Literatur-Parse-Methoden.
    git-svn-id: https://moonvega.pmhost.de/svn/meistergeister/trunk@1406 c5e1f230-26d4-41f2-a5c8-149495a0d85e

---
+++ Model/Literatur.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MeisterGeister.Logic.Literatur;
+
+namespace MeisterGeister.Model
+{
+    public partial class Literatur
+    {
+        public Literatur()
+        {
+            LiteraturGUID = Guid.NewGuid();
+        }
+
+        public bool Usergenerated
+        {
+            get { return !LiteraturGUID.ToString().StartsWith("00000000-0000-0000-00"); }
+        }
+
+        private static Parser parser = null;
+        private static Parser Parser
+        {
+            get {
+                if (parser == null)
+                    parser = new Parser(Scanner);
+                return Literatur.parser; 
+            }
+        }
+        private static Scanner scanner = null;
+        private static Scanner Scanner
+        {
+            get {
+                if (scanner == null)
+                    scanner = new Scanner();
+                return Literatur.scanner; 
+            }
+        }
+
+        public static List<Literaturangabe> Parse(ILiteratur ilit)
+        {
+            if (ilit == null || ilit.Literatur == null)
+                return null;
+            return Parse(ilit.Literatur);
+        }
+
+        public static List<Literaturangabe> Parse(string lit)
+        {
+            if (lit == null)
+                return null;
+            ParseTree tree = Literatur.Parser.Parse(lit);
+            if (tree.Errors.Count > 0)
+                return null;
+            return (List<Literaturangabe>)tree.Eval();
+        }
+    }
+}
