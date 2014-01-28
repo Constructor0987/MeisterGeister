eafc421e46cee1eb23a90d760b12b3e7f88fba69
markus <markus@c5e1f230-26d4-41f2-a5c8-149495a0d85e>
2014-01-28 11:44:53 +0000

    Control zum Öffnen von Literaturangaben erstellt und in den Basar eingebaut. Ist aber noch nicht fertig... see #98
    git-svn-id: https://moonvega.pmhost.de/svn/meistergeister/trunk@1596 c5e1f230-26d4-41f2-a5c8-149495a0d85e

---
+++ View/General/LiteraturControl.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MeisterGeister.View.General
+{
+    /// <summary>
+    /// Interaktionslogik für LiteraturControl.xaml
+    /// </summary>
+    public partial class LiteraturControl : UserControl
+    {
+        public LiteraturControl()
+        {
+            InitializeComponent();
+        }
+
+        public string Literaturangabe
+        {
+            get { return (string)GetValue(LiteraturangabeProperty); }
+            set { SetValue(LiteraturangabeProperty, value); }
+        }
+        public static readonly DependencyProperty LiteraturangabeProperty = DependencyProperty.Register(
+          "Literaturangabe", typeof(string), typeof(LiteraturControl));
+
+        private void LiteraturControl_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            List<Logic.Literatur.Literaturangabe> literaurList = Model.Literatur.Parse(Literaturangabe);
+
+            _contextMenu.Items.Clear();
+
+            foreach (var item in literaurList)
+            {
+                MenuItem menuItem = new MenuItem();
+                menuItem.Tag = item;
+                menuItem.Header = item.ToString();
+                menuItem.Click += menuItem_Click;
+                _contextMenu.Items.Add(menuItem);
+            }
+
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                _contextMenu.PlacementTarget = this;
+                _contextMenu.IsOpen = true;
+                e.Handled = true;
+            }
+        }
+
+        void menuItem_Click(object sender, RoutedEventArgs e)
+        {
+            // TODO (markus): PDF öffnen
+            if (sender is MenuItem)
+            {
+                object liObject = ((MenuItem)sender).Tag;
+                if (liObject != null && liObject is Logic.Literatur.Literaturangabe)
+                {
+                    Logic.Literatur.Literaturangabe literaturangabe = (Logic.Literatur.Literaturangabe)liObject;
+
+                    // Auf Basis der Literaturangabe muss das passende PDF mit Dateipfad gesucht werden.
+                    // Ist kein PDF hinterlegt, sollte ein FileOpen-Dialog erscheinen oder ein Link zum Ulisses-PDF-Shop angezeigt werden.
+                    // Sollten bei einer Literaturangabe mehrere Seiten angegeben sein, muss der User eine auswählen.
+
+                    MessageBox.Show("Jetzt müsste ein PDF geöffnet werden...");
+                    Logic.General.Pdf.OpenFileInReader("PFAD", literaturangabe.Seiten.FirstOrDefault().Seite);
+                }
+            }
+        }
+    }
+}
---
+++ View/General/StringEmptyToVisibilityConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace MeisterGeister.View.General
+{
+    public class StringEmptyToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value != null && value is String && ((String)value) != String.Empty)
+                return Visibility.Visible;
+            else
+                return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // keine Rückkonvertierung vorgesehen
+            return null;
+        }
+    }
+
+}
