bd1fc2e756f5e7bc511e3a7992638c660d4d6e04
markus <markus@c5e1f230-26d4-41f2-a5c8-149495a0d85e>
2014-02-23 19:27:08 +0000

    FileExtensions.cs nicht eingecheckt
    git-svn-id: https://moonvega.pmhost.de/svn/meistergeister/trunk@1719 c5e1f230-26d4-41f2-a5c8-149495a0d85e

---
+++ Logic/Extensions/FileExtensions.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeisterGeister.Logic.Extensions
+{
+    public static class FileExtensions
+    {
+        /// <summary>
+        /// Wandelt 'path' in eine relative Pfadangabe in Relation zum MeisterGeister-Verzeichnis um.
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        public static string ConvertAbsoluteToRelativePath(string path)
+        {
+            Uri file = new Uri(path);
+            Uri homePath = new Uri(GetHomeDirectory());
+            Uri relativePath = homePath.MakeRelativeUri(file);
+            return Uri.UnescapeDataString(relativePath.ToString()).Replace("/", "\\").Insert(0, "\\");
+        }
+
+        //if (fileName.StartsWith("\\")) // bei relativem Pfad das HomeDir hinzufügen
+        //        fileName = fileName.Remove(0, 1).Insert(0, Logic.Extensions.FileExtensions.GetHomeDirectory());
+
+        /// <summary>
+        /// Gibt das MeisterGeister Stammverzeichnis als String zurück.
+        /// </summary>
+        /// <returns></returns>
+        public static string GetHomeDirectory()
+        {
+            return System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location) + "\\";
+        }
+
+        /// <summary>
+        /// Setzt das CurrentDirectory auf das MeisterGeister Stammverzeichnis.
+        /// </summary>
+        public static void SetCurrentDir()
+        {
+            Environment.CurrentDirectory = Logic.Extensions.FileExtensions.GetHomeDirectory();
+        }
+
+        /// <summary>
+        /// Setzt das CurrentDirectory auf "path'.
+        /// </summary>
+        /// <param name="path"></param>
+        public static void SetCurrentDir(string path)
+        {
+            Environment.CurrentDirectory = System.IO.Path.GetDirectoryName(path);
+        }
+
+    }
+}
