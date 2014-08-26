1c6b36e9f682484739ab90f33289ee0098eada8c
markus <markus@c5e1f230-26d4-41f2-a5c8-149495a0d85e>
2014-08-26 06:27:52 +0000

    Release Intern 2.4.0.8 Basar: Umrechnung in andere Währungen fix #233
    git-svn-id: https://moonvega.pmhost.de/svn/meistergeister/trunk@1828 c5e1f230-26d4-41f2-a5c8-149495a0d85e

--- Daten/Updateskripte/UpdateTo_V0091.sql
+++ Daten/Updateskripte/UpdateTo_V0091.sql
@@ -1,7 +1,3 @@
-﻿--Daten-Update: TODO - Gegner Uthuria und Untote
-
---Daten-Update: TODO - Rassen, Kulturen und Namen (vor allem für Riesland, Myranor und Uthuria)
-
---Audio Datei in allen Verzeichnissen suchen wenn nicht gefunden wie angegeben (Performance)
+﻿--Audio Datei in allen Verzeichnissen suchen wenn nicht gefunden wie angegeben (Performance)
 INSERT INTO [Einstellung] (Kontext, Name, Typ, Wert) VALUES ('Audioplayer', 'AudioInAnderemPfadSuchen', 'Boolean','1');
 
---
+++ Daten/Updateskripte/UpdateTo_V0092.sql
@@ -0,0 +1,4 @@
+﻿--Daten-Update: TODO - Gegner Uthuria und Untote
+
+--Daten-Update: TODO - Rassen, Kulturen und Namen (vor allem für Riesland, Myranor und Uthuria)
+
