85f80d78a5f0f1f6eba8b5b98cb69b6a2e8271bf
markus <markus@c5e1f230-26d4-41f2-a5c8-149495a0d85e>
2014-11-05 14:52:21 +0000

    BugFix: Nachteil Glasknochen war falsch als Vorteil markiert. fix #381
    git-svn-id: https://moonvega.pmhost.de/svn/meistergeister/trunk@1903 c5e1f230-26d4-41f2-a5c8-149495a0d85e

--- Daten/Updateskripte/UpdateTo_V0093.sql
+++ Daten/Updateskripte/UpdateTo_V0093.sql
@@ -1,4 +1,7 @@
-﻿-- Platzhalter-Datei für:
+﻿--- Korrektur Nachteil Glasknochen
+UPDATE [VorNachteil] SET [Vorteil] = 0, [Nachteil] = 1 WHERE [VorNachteilGUID] = '00000000-0000-0000-f024-000000000002';
+
+-- Platzhalter-Datei für:
 
 --Daten-Update: TODO - Gegner Uthuria und Untote
 
