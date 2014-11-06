2b69a8ccd0e671baf0f0eb54ec6e7c4ee072352e
markus <markus@c5e1f230-26d4-41f2-a5c8-149495a0d85e>
2014-11-06 07:48:45 +0000

    SQL-Skript 93 aufgeteilt in: "Audio" und "Daten-Updates". Weitere Statements können in beiden Dateien ergänzt werden.
    git-svn-id: https://moonvega.pmhost.de/svn/meistergeister/trunk@1906 c5e1f230-26d4-41f2-a5c8-149495a0d85e

--- Daten/DatabaseUpdate.cs
+++ Daten/DatabaseUpdate.cs
@@ -18,7 +18,7 @@ namespace MeisterGeister.Daten
         /// <summary>
         /// Die aktuell benötigte Datenbank-Version.
         /// </summary>
-        public const int DatenbankVersionAktuell = 93;
+        public const int DatenbankVersionAktuell = 94;
 
         private const string DatabasePwd = ";Password=m3ist3rg3ist3r;Persist Security Info=False";
 
--- Daten/Updateskripte/UpdateTo_V0093.sql
+++ Daten/Updateskripte/UpdateTo_V0093.sql
@@ -1,12 +1,6 @@
-﻿--- Korrektur Nachteil Glasknochen
-UPDATE [VorNachteil] SET [Vorteil] = 0, [Nachteil] = 1 WHERE [VorNachteilGUID] = '00000000-0000-0000-f024-000000000002';
--- AudioTool Repeat mit Repeat nur ein Titel
+﻿-- AudioTool Repeat mit Repeat nur ein Titel
 ALTER TABLE [Audio_Playlist] ALTER COLUMN [Repeat] bit NULL;
 -- AudioTool Hotkey Slider Volume speichern
 INSERT INTO [Einstellung] (Kontext, Name, Typ, Wert) VALUES ('Audioplayer', 'GeneralHotkeyVolume', 'Integer','50');
 
--- Platzhalter-Datei für:
-
---Daten-Update: TODO - Gegner Uthuria und Untote
-
---Daten-Update: TODO - Rassen, Kulturen und Namen (vor allem für Riesland, Myranor und Uthuria)
+-- evtl. weitere Updates ergänzen...
