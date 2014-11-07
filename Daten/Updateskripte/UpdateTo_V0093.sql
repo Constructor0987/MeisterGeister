aa725a0afeb815f11f760329b4bff18084bdf56d
markus <markus@c5e1f230-26d4-41f2-a5c8-149495a0d85e>
2014-11-07 12:26:07 +0000

    AudioPlayer: Fading Einstellung der Playlisten überarbeitet. testing #437
    git-svn-id: https://moonvega.pmhost.de/svn/meistergeister/trunk@1912 c5e1f230-26d4-41f2-a5c8-149495a0d85e

--- Daten/Updateskripte/UpdateTo_V0093.sql
+++ Daten/Updateskripte/UpdateTo_V0093.sql
@@ -2,5 +2,9 @@
 ALTER TABLE [Audio_Playlist] ALTER COLUMN [Repeat] bit NULL;
 -- AudioTool Hotkey Slider Volume speichern
 INSERT INTO [Einstellung] (Kontext, Name, Typ, Wert) VALUES ('Audioplayer', 'GeneralHotkeyVolume', 'Integer','50');
+-- AudioTool Fading Werte setzen
+UPDATE [Audio_Playlist] SET [Fading] = 0 WHERE [Länge] < ANY
+    (SELECT CONVERT(float, CONVERT(nvarchar, [Wert])) * 2 FROM 
+        [Einstellung] WHERE [Kontext] = 'Audioplayer' AND [Name] = 'Fading');
 
 -- evtl. weitere Updates ergänzen...
