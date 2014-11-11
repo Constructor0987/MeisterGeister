e147db5eaa452b96190c1f07cbf66d462b58f021
markus <markus@c5e1f230-26d4-41f2-a5c8-149495a0d85e>
2014-11-11 08:55:38 +0000

    Neue Sonderfertigkeiten aus der Gareth-Box: Schattenpfade. fix #448
    git-svn-id: https://moonvega.pmhost.de/svn/meistergeister/trunk@1933 c5e1f230-26d4-41f2-a5c8-149495a0d85e

--- Daten/Updateskripte/UpdateTo_V0094.sql
+++ Daten/Updateskripte/UpdateTo_V0094.sql
@@ -1,8 +1,2 @@
 ﻿--- Korrektur Nachteil Glasknochen
 UPDATE [VorNachteil] SET [Vorteil] = 0, [Nachteil] = 1 WHERE [VorNachteilGUID] = '00000000-0000-0000-f024-000000000002';
-
--- eventuell weitere Daten-Updates ergänzen...
-
---Daten-Update: TODO - Gegner Uthuria und Untote
-
---Daten-Update: TODO - Rassen, Kulturen und Namen (vor allem für Riesland, Myranor und Uthuria)
---
+++ Daten/Updateskripte/UpdateTo_V0095.sql
@@ -0,0 +1,30 @@
+﻿--- Neue Sonderfertigkeiten
+INSERT INTO [Sonderfertigkeit] (  [SonderfertigkeitGUID],  [Name],  [HatWert],  [Typ],  [Literatur],  [Voraussetzungen]) 
+ VALUES ('00000000-0000-0000-005f-000000001658' ,N'Schattenpfade I' ,0 ,N'Magisch' ,N'Goldene Dächer, düstere Gassen 185' ,N'Gassenwissen 7, SF Ortkenntnis');
+INSERT INTO [Sonderfertigkeit] (  [SonderfertigkeitGUID],  [Name],  [HatWert],  [Typ],  [Literatur],  [Voraussetzungen]) 
+ VALUES ('00000000-0000-0000-005f-000000001659' ,N'Schattenpfade II' ,0 ,N'Magisch' ,N'Goldene Dächer, düstere Gassen 185' ,N'Gassenwissen 9, SF Ortkenntnis');
+INSERT INTO [Sonderfertigkeit] (  [SonderfertigkeitGUID],  [Name],  [HatWert],  [Typ],  [Literatur],  [Voraussetzungen]) 
+ VALUES ('00000000-0000-0000-005f-000000001660' ,N'Schattenpfade III' ,0 ,N'Magisch' ,N'Goldene Dächer, düstere Gassen 185' ,N'Gassenwissen 13, SF Ortkenntnis');
+INSERT INTO [Sonderfertigkeit] (  [SonderfertigkeitGUID],  [Name],  [HatWert],  [Typ],  [Literatur],  [Voraussetzungen]) 
+ VALUES ('00000000-0000-0000-005f-000000001661' ,N'Schattenpfade IV' ,0 ,N'Magisch' ,N'Goldene Dächer, düstere Gassen 185' ,N'Gassenwissen 15, SF Ortkenntnis');
+INSERT INTO [Sonderfertigkeit] (  [SonderfertigkeitGUID],  [Name],  [HatWert],  [Typ],  [Literatur],  [Voraussetzungen]) 
+ VALUES ('00000000-0000-0000-005f-000000001662' ,N'Berufsgeheimnis: Inrah' ,0 ,N'Allgemein' ,N'Kartenglück und Schicksalszeichen 184' ,N'KL 11, IN 13, V Prophezeien');
+
+ INSERT INTO [Sonderfertigkeit_Setting] (  [SonderfertigkeitGUID],  [SettingGUID],  [Verbreitung],  [Name]) 
+ VALUES ('00000000-0000-0000-005f-000000001658' ,'00000000-0000-0000-5e77-000000000001' ,NULL ,NULL);
+INSERT INTO [Sonderfertigkeit_Setting] (  [SonderfertigkeitGUID],  [SettingGUID],  [Verbreitung],  [Name]) 
+ VALUES ('00000000-0000-0000-005f-000000001659' ,'00000000-0000-0000-5e77-000000000001' ,NULL ,NULL);
+INSERT INTO [Sonderfertigkeit_Setting] (  [SonderfertigkeitGUID],  [SettingGUID],  [Verbreitung],  [Name]) 
+ VALUES ('00000000-0000-0000-005f-000000001660' ,'00000000-0000-0000-5e77-000000000001' ,NULL ,NULL);
+INSERT INTO [Sonderfertigkeit_Setting] (  [SonderfertigkeitGUID],  [SettingGUID],  [Verbreitung],  [Name]) 
+ VALUES ('00000000-0000-0000-005f-000000001661' ,'00000000-0000-0000-5e77-000000000001' ,NULL ,NULL);
+INSERT INTO [Sonderfertigkeit_Setting] (  [SonderfertigkeitGUID],  [SettingGUID],  [Verbreitung],  [Name]) 
+ VALUES ('00000000-0000-0000-005f-000000001662' ,'00000000-0000-0000-5e77-000000000001' ,NULL ,NULL);
+
+
+
+-- eventuell weitere Daten-Updates ergänzen...
+
+--Daten-Update: TODO - Gegner Uthuria und Untote
+
+--Daten-Update: TODO - Rassen, Kulturen und Namen (vor allem für Riesland, Myranor und Uthuria)
