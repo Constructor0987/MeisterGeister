89a21be0971f0b283b681bbdbeb4fe57eb2b1347
markus <markus@c5e1f230-26d4-41f2-a5c8-149495a0d85e>
2014-11-13 09:54:46 +0000

    Daten-Uodate für Kampfregeln. fix #457
    git-svn-id: https://moonvega.pmhost.de/svn/meistergeister/trunk@1945 c5e1f230-26d4-41f2-a5c8-149495a0d85e

--- Daten/DatabaseUpdate.cs
+++ Daten/DatabaseUpdate.cs
@@ -18,7 +18,7 @@ namespace MeisterGeister.Daten
         /// <summary>
         /// Die aktuell benötigte Datenbank-Version.
         /// </summary>
-        public const int DatenbankVersionAktuell = 95;
+        public const int DatenbankVersionAktuell = 96;
 
         private const string DatabasePwd = ";Password=m3ist3rg3ist3r;Persist Security Info=False";
 
