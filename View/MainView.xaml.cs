22f1f1b96cf290f9e0c5efa9b5bfbf940fb01b59
markus <markus@c5e1f230-26d4-41f2-a5c8-149495a0d85e>
2014-11-13 10:42:01 +0000

    BugFix AutoUpdateCheck
    git-svn-id: https://moonvega.pmhost.de/svn/meistergeister/trunk@1947 c5e1f230-26d4-41f2-a5c8-149495a0d85e

--- View/MainView.xaml.cs
+++ View/MainView.xaml.cs
@@ -502,7 +502,7 @@ namespace MeisterGeister.View
 
 #if !(DEBUG)
             // UpdateCheck (nicht ausführen, wenn von IDE ausgeführt)
-            if (System.Diagnostics.Debugger.IsAttached == true && Einstellungen.CheckForUpdates && Einstellungen.LastUpdateCheck.CompareTo(DateTime.Now.Date) != 0)
+            if (System.Diagnostics.Debugger.IsAttached == false && Einstellungen.CheckForUpdates && Einstellungen.LastUpdateCheck.CompareTo(DateTime.Now.Date) != 0)
                 CheckForUpdates(false);
 
             // ChangeLog Meldung (nicht ausführen, wenn von IDE ausgeführt)
