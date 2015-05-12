858b78e8bbc59f843a7f8efb1d1cc72142a082dc
jonas <jonas@c5e1f230-26d4-41f2-a5c8-149495a0d85e>
2015-05-12 19:54:46 +0000

    Dlls ins Postbuild übernommen. AlchimieView Anfänge vom UI aufräumen. Datei für das CustomModifikatorViewModel angelegt. Alte ImpromptuInterface.dll gelöscht.
    git-svn-id: https://moonvega.pmhost.de/svn/meistergeister/trunk@2005 c5e1f230-26d4-41f2-a5c8-149495a0d85e

---
+++ ViewModel/Modifikatoren/CustomModifikatorViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MeisterGeister.ViewModel.Kampf.Logic.Modifikatoren;
+
+namespace MeisterGeister.ViewModel.Modifikatoren
+{
+    public class CustomModifikatorViewModel
+    {
+        private CustomModifikatorFactory factory;
+    }
+}
