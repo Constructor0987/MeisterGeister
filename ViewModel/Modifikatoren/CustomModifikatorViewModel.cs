4db51d195bd55bd08231aaffb4393564c07c2cfb
jonas <jonas@c5e1f230-26d4-41f2-a5c8-149495a0d85e>
2015-05-12 21:09:51 +0000

    CustomModifikatorViewModel vorgedacht.
    git-svn-id: https://moonvega.pmhost.de/svn/meistergeister/trunk@2006 c5e1f230-26d4-41f2-a5c8-149495a0d85e

--- ViewModel/Modifikatoren/CustomModifikatorViewModel.cs
+++ ViewModel/Modifikatoren/CustomModifikatorViewModel.cs
@@ -10,5 +10,33 @@ namespace MeisterGeister.ViewModel.Modifikatoren
     public class CustomModifikatorViewModel
     {
         private CustomModifikatorFactory factory;
+        
+        //Basisdaten
+        // Liste von möglichen Modifikatoren (gruppiert nach Typ)
+        // Liste von Rechenfunktionen
+        // Liste von Zaubernamen
+        // Liste von Talentnamen
+
+        //Liste von eingebrachten Modifikatoren
+        // je Modifikator:
+        // Rechenfunktion und Wert
+        // Beim EndetMitZeitpunkt-Modifikator muss später noch ein DSA-Datum hinzugefügt werden. Das setzt aber einen überarbeiteten Kalender voraus.
+
+        //Auswirkungstext
+        //Fehler
+        //Name (editierbar)
+        //Literaturangabe (editierbar)
+        //0-n Zaubername
+        //0-n Talentname
+
+        //Aktionen
+        // Modifikator hinzufügen
+        //  mit check ob schon vorhanden?
+        // Talentname hinzufügen
+        // Talentname löschen
+        // Zaubername hinzufügen
+        // Zaubername löschen
+        // Modifikator löschen
+        // Anwenden (ist danach nicht mehr editierbar)
     }
 }
