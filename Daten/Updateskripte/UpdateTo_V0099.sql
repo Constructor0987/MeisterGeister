847b9f2ace1a4237cf6d2cf9e70ab13eabb00a04
jonas <jonas@c5e1f230-26d4-41f2-a5c8-149495a0d85e>
2015-05-22 23:01:11 +0000

    Entwurf für Modifikator-Tabellen
    git-svn-id: https://moonvega.pmhost.de/svn/meistergeister/trunk@2018 c5e1f230-26d4-41f2-a5c8-149495a0d85e

---
+++ Daten/Updateskripte/UpdateTo_V0099.sql
@@ -0,0 +1,32 @@
+﻿-- Modifkator-Tabellen hinzufügen
+CREATE TABLE [Modifikator] (
+	[ModifikatorGUID] uniqueidentifier NOT NULL DEFAULT newid() PRIMARY KEY, 
+	[Name] nvarchar(100) NOT NULL, 
+	[Typ] nvarchar(200) NOT NULL,
+	[Beschreibung] ntext,
+	[Json] ntext,
+	[Literatur] nvarchar(300)
+)
+GO
+CREATE UNIQUE INDEX [UQ_Modifikator]
+	ON [Modifikator] (
+	[Name]
+)
+GO
+CREATE TABLE [Held_Modifikator] (
+	[Id] bigint NOT NULL IDENTITY PRIMARY KEY, 
+	[HeldGUID] uniqueidentifier NOT NULL DEFAULT '00000000-0000-0000-0000-000000000000', 
+	[ModifikatorGUID] uniqueidentifier NOT NULL DEFAULT '00000000-0000-0000-0000-000000000000', 
+	[Wert1] int NULL, 
+	[Wert2] int NULL, 
+	[Wert3] int NULL,
+	[Erstellt] datetime NOT NULL DEFAULT GETDATE(),
+	FOREIGN KEY ([HeldGUID])
+		REFERENCES [Held] ([HeldGUID])
+		ON UPDATE CASCADE ON DELETE CASCADE, 
+	FOREIGN KEY ([ModifikatorGUID])
+		REFERENCES [Modifikator] ([ModifikatorGUID])
+		ON UPDATE CASCADE ON DELETE CASCADE
+)
+GO
+--TODO MT: Anpassungen an Literatur-Tabellen
