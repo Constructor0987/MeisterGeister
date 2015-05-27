cec2408c9314b30e524debd9a23c075f6cf3d510
markus <markus@c5e1f230-26d4-41f2-a5c8-149495a0d85e>
2015-05-27 11:44:04 +0000

    Höhere Stufen des Vorteils "Wesen der Nacht" waren im Setting "Aventurien" nicht verfügbar. fix #521
    git-svn-id: https://moonvega.pmhost.de/svn/meistergeister/trunk@2020 c5e1f230-26d4-41f2-a5c8-149495a0d85e

--- Daten/Updateskripte/UpdateTo_V0099.sql
+++ Daten/Updateskripte/UpdateTo_V0099.sql
@@ -1,4 +1,7 @@
-﻿--Anpassungen an Literatur-Tabellen
+﻿-- Fehler in Vorteil korrigieren
+UPDATE [VorNachteil] SET [Setting] = 'Aventurien, Myranor', [Literatur] = 'WdH 258 / WnM 120' WHERE [Name] LIKE 'Wesen der Nacht%';
+
+--Anpassungen an Literatur-Tabellen
 ALTER TABLE [Literatur] ADD [Regelsystem] int NOT NULL DEFAULT 0;
 ALTER TABLE [Literatur] ADD [Nummer] nvarchar(50) NULL;
 ALTER TABLE [Literatur] ADD [Art] nvarchar(150) NULL;
