a0db323fa3d3f5cc5ed562a5b698d9f817c2491f
bbrecht <bbrecht@c5e1f230-26d4-41f2-a5c8-149495a0d85e>
2015-07-19 13:44:38 +0000

    GUI für das neue Beschwörungstool erstellt (das alte umbenannt) GUI des Probentools leicht verbessert. Man sieht jetzt alle Proben-Mods und muss nicht mehr hässlich scrollen.
    git-svn-id: https://moonvega.pmhost.de/svn/meistergeister/trunk@2044 c5e1f230-26d4-41f2-a5c8-149495a0d85e

--- View/Beschwörung/BeschwörungView.xaml.cs
+++ View/Beschwörung/BeschwörungView.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -11,10 +12,6 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-//Eigene Usings
-using VM = MeisterGeister.ViewModel.Beschwörung;
-//Weitere Usings
-using System.Diagnostics;
 
 namespace MeisterGeister.View.Beschwörung
 {
@@ -26,24 +23,6 @@ namespace MeisterGeister.View.Beschwörung
         public BeschwörungView()
         {
             InitializeComponent();
-        }
-
-        private void UserControl_Loaded(object sender, RoutedEventArgs e)
-        {
-            try
-            {
-                (this.DataContext as VM.BeschwörungViewModel).LoadDaten();
-            }
-            catch (Exception ex)
-            {
-                View.Windows.MsgWindow errWin = new View.Windows.MsgWindow("Beschwörung-Tool", "Beim Laden des Beschwörung-Tools ist ein Fehler aufgetreten.", ex);
-                errWin.ShowDialog();
-            }
-        }
-
-        private void Expander_Expanded(object sender, RoutedEventArgs e)
-        {
-
         }
     }
 }
---
+++ View/Beschwörung/OldBeschwörungView.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+//Eigene Usings
+using VM = MeisterGeister.ViewModel.Beschwörung;
+//Weitere Usings
+using System.Diagnostics;
+
+namespace MeisterGeister.View.Beschwörung
+{
+    /// <summary>
+    /// Interaktionslogik für BeschwörungView.xaml
+    /// </summary>
+    public partial class OldBeschwörungView : UserControl
+    {
+        public OldBeschwörungView()
+        {
+            InitializeComponent();
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                (this.DataContext as VM.OldBeschwörungViewModel).LoadDaten();
+            }
+            catch (Exception ex)
+            {
+                View.Windows.MsgWindow errWin = new View.Windows.MsgWindow("Beschwörung-Tool", "Beim Laden des Beschwörung-Tools ist ein Fehler aufgetreten.", ex);
+                errWin.ShowDialog();
+            }
+        }
+
+        private void Expander_Expanded(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
