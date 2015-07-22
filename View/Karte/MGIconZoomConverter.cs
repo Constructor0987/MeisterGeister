96f0e774ad637f2e8ced53095e47ff023216a451
jonas <jonas@c5e1f230-26d4-41f2-a5c8-149495a0d85e>
2015-07-22 08:33:21 +0000

    Fehlende Dateien für die Karte.
    git-svn-id: https://moonvega.pmhost.de/svn/meistergeister/trunk@2053 c5e1f230-26d4-41f2-a5c8-149495a0d85e

---
+++ View/Karte/MGIconZoomConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+
+namespace MeisterGeister.View.Karte
+{
+    [ValueConversion(typeof(double), typeof(double))]
+    public class MGIconZoomConverter : IValueConverter
+    {
+        #region IValueConverter Members
+
+        public object Convert(object value, Type targetType, object parameter,
+            System.Globalization.CultureInfo culture)
+        {
+            if (targetType != typeof(double) && targetType != typeof(double?))
+                throw new InvalidOperationException("The target must be a double");
+
+            return 25.0 * 1.0/(double)value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter,
+            System.Globalization.CultureInfo culture)
+        {
+            return Convert(value, targetType, parameter, culture);
+        }
+
+        #endregion
+    }
+}
