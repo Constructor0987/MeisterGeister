00b539801ee71585fca8ada1dda34170360879f4
jonas <jonas@c5e1f230-26d4-41f2-a5c8-149495a0d85e>
2015-07-22 07:59:11 +0000

    Fehlende Dateien für die Karte.
    git-svn-id: https://moonvega.pmhost.de/svn/meistergeister/trunk@2052 c5e1f230-26d4-41f2-a5c8-149495a0d85e

---
+++ View/General/TrackBehavior.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace MeisterGeister.View.General
+{
+    public static class TrackBehavior
+    {
+        public static readonly DependencyProperty TrackOpenLocationProperty = DependencyProperty.RegisterAttached("TrackOpenLocation", typeof(bool), typeof(TrackBehavior), new UIPropertyMetadata(false, OnTrackOpenLocationChanged));
+
+        public static bool GetTrackOpenLocation(ContextMenu item)
+        {
+            return (bool)item.GetValue(TrackOpenLocationProperty);
+        }
+
+        public static void SetTrackOpenLocation(ContextMenu item, bool value)
+        {
+            item.SetValue(TrackOpenLocationProperty, value);
+        }
+
+        public static readonly DependencyProperty OpenLocationProperty = DependencyProperty.RegisterAttached("OpenLocation", typeof(Point), typeof(TrackBehavior), new UIPropertyMetadata(new Point()));
+
+        public static Point GetOpenLocation(ContextMenu item)
+        {
+            return (Point)item.GetValue(OpenLocationProperty);
+        }
+
+        public static void SetOpenLocation(ContextMenu item, Point value)
+        {
+            item.SetValue(OpenLocationProperty, value);
+        }
+
+        static void OnTrackOpenLocationChanged(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs e)
+        {
+            var menu = dependencyObject as ContextMenu;
+            if (menu == null)
+            {
+                return;
+            }
+
+            if (!(e.NewValue is bool))
+            {
+                return;
+            }
+
+            if ((bool)e.NewValue)
+            {
+                menu.Opened += menu_Opened;
+
+            }
+            else
+            {
+                menu.Opened -= menu_Opened;
+            }
+        }
+
+        static void menu_Opened(object sender, RoutedEventArgs e)
+        {
+            if (!ReferenceEquals(sender, e.OriginalSource))
+            {
+                return;
+            }
+
+            var menu = e.OriginalSource as ContextMenu;
+            if (menu != null)
+            {
+                SetOpenLocation(menu, Mouse.GetPosition(menu.PlacementTarget));
+            }
+        }
+    }
+}
---
+++ ViewModel/Karte/Logic/Karte.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeisterGeister.ViewModel.Karte.Logic
+{
+    public class Karte
+    {
+        string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        int breite, höhe;
+
+        public int Höhe
+        {
+            get { return höhe; }
+            set { höhe = value; }
+        }
+
+        public int Breite
+        {
+            get { return breite; }
+            set { breite = value; }
+        }
+        string bildpfad;
+
+        public string Bildpfad
+        {
+            get { return bildpfad; }
+            set { bildpfad = value; }
+        }
+
+        public Karte(string name, string bildpfad, int breite, int höhe)
+        {
+            Name = name;
+            Bildpfad = bildpfad;
+            Breite = breite;
+            Höhe = höhe;
+        }
+    }
+}
