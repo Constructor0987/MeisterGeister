47c74ebb4cf17b916b38052aaf029790f8e07c8f
jonas <jonas@c5e1f230-26d4-41f2-a5c8-149495a0d85e>
2015-10-03 19:53:20 +0000

    Svn probleme mit Case
    git-svn-id: https://moonvega.pmhost.de/svn/meistergeister/trunk@2168 c5e1f230-26d4-41f2-a5c8-149495a0d85e

---
+++ ViewModel/AudioPlayer/Logic/IWesenPlaylist.cs
@@ -0,0 +1,17 @@
+﻿using MeisterGeister.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MeisterGeister.ViewModel.AudioPlayer.Logic
+{
+    public interface IWesenPlaylist
+    {
+        bool HatGegner { get; }
+        bool HatHeld { get; }
+        Audio_Playlist Audio_Playlist { get; }
+        GegnerBase GegnerBase { get; }
+        Held Held { get; }
+
+    }
+}
