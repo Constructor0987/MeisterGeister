556f1ac4028f5c270eb10848bf928cd2dcf5788c
jonas <jonas@c5e1f230-26d4-41f2-a5c8-149495a0d85e>
2015-10-08 21:58:36 +0000

    VM und Entwurf für ein UserControl für die Auswahl von Landschaften anhand der Gruppe.
    git-svn-id: https://moonvega.pmhost.de/svn/meistergeister/trunk@2189 c5e1f230-26d4-41f2-a5c8-149495a0d85e

---
+++ View/ZooBot/LandschaftenView.xaml.cs
@@ -0,0 +1,66 @@
+﻿using MeisterGeister.View.General;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+// Eigene Usings
+using VM = MeisterGeister.ViewModel.ZooBot.Logic;
+
+namespace MeisterGeister.View.ZooBot
+{
+    /// <summary>
+    /// Interaktionslogik für LandschaftenView.xaml
+    /// </summary>
+    public partial class LandschaftenView : UserControl
+    {
+        public IList<Model.Landschaft> SelectedItems
+        {
+            get { return (IList<Model.Landschaft>)GetValue(SelectedItemsProperty); }
+            set { SetValue(SelectedItemsProperty, value); }
+        }
+        public static DependencyProperty SelectedItemsProperty = DependencyProperty.Register("SelectedItems", typeof(IList<Model.Landschaft>), typeof(LandschaftenView),
+                new PropertyMetadata(false));
+
+
+        public VM.LandschaftenVM VM
+        {
+            get
+            {
+                if (DataContext == null || !(DataContext is VM.LandschaftenVM))
+                    return null;
+                return DataContext as VM.LandschaftenVM;
+            }
+            set
+            {
+                DataContext = value;
+            }
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+        }
+
+        public LandschaftenView()
+        {
+            InitializeComponent();
+            VM = new VM.LandschaftenVM();
+            VM.PropertyChanged += VM_PropertyChanged;
+            
+        }
+
+        void VM_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            var vm = sender as VM.LandschaftenVM;
+            if (sender != null && e.PropertyName == "SelectedItems")
+                SelectedItems = vm.SelectedItems;
+        }
+    }
+}
---
+++ ViewModel/ZooBot/Logic/LandschaftenVM.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MeisterGeister.Model;
+using MeisterGeister.Logic.Extensions;
+
+namespace MeisterGeister.ViewModel.ZooBot.Logic
+{
+    public class LandschaftenVM : Base.ViewModelBase
+    {
+        Dictionary<Guid, LandschaftItem> landschaftItems = new Dictionary<Guid, LandschaftItem>();
+
+        public LandschaftenVM()
+        {
+            //Landschaften laden
+            Landschaftsgruppen = new List<LandschaftsgruppeItem>();
+            foreach(var lg in Global.ContextHeld.Liste<Landschaftsgruppe>())
+            {
+                var lgi = new LandschaftsgruppeItem(lg, this);
+                Landschaftsgruppen.Add(lgi);
+                lgi.PropertyChanged += lgi_PropertyChanged;
+            }
+        }
+
+        void lgi_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            var lg = sender as LandschaftsgruppeItem;
+            if (lg != null && e.PropertyName == "IsSelected")
+                OnChanged("SelectedItems");
+        }
+
+        public Dictionary<Guid, LandschaftItem> LandschaftItems
+        {
+            get { return landschaftItems; }
+            protected set { Set(ref landschaftItems, value); }
+        }
+
+        public IList<Landschaft> SelectedItems
+        {
+            get { return LandschaftItems.Values.Where(l => l.IsSelected).Select(l => l.Landschaft).ToList(); }
+        }
+
+        //Für die Anzeige
+        public IList<LandschaftsgruppeItem> Landschaftsgruppen
+        { get; set; }
+
+    }
+
+    public class LandschaftItem : Base.ViewModelBase
+    {
+        public LandschaftItem(Landschaft l)
+        {
+            Landschaft = l;
+        }
+
+        private Landschaft landschaft;
+        public Landschaft Landschaft
+        {
+            get { return landschaft; }
+            set { landschaft = value; }
+        }
+
+        private bool isSelected = false;
+        public bool IsSelected
+        {
+            get { return isSelected; }
+            set { Set(ref isSelected, value); }
+        }
+    }
+
+    public class LandschaftsgruppeItem : Base.ViewModelBase
+    {
+        public LandschaftsgruppeItem(Landschaftsgruppe g, LandschaftenVM parentVM)
+        {
+            Landschaftsgruppe = g;
+            Landschaften = new ExtendedObservableCollection<LandschaftItem>();
+            foreach(var l in g.Landschaft)
+            {
+                LandschaftItem li = null;
+                if (parentVM.LandschaftItems.ContainsKey(l.LandschaftGUID))
+                    li = parentVM.LandschaftItems[l.LandschaftGUID];
+                else
+                {
+                    li = new LandschaftItem(l);
+                    parentVM.LandschaftItems.Add(l.LandschaftGUID, li);
+                }
+                li.PropertyChanged += li_PropertyChanged;
+                Landschaften.Add(li);
+            }
+        }
+
+        bool ignoreChangedEvent = false;
+        void li_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            if (ignoreChangedEvent)
+                return;
+            var li = sender as LandschaftItem;
+            if(li != null && e.PropertyName == "IsSelected")
+            {
+                bool allSelected = li.IsSelected, noneSelected = !li.IsSelected;
+                foreach(var l in Landschaften)
+                {
+                    if (l.IsSelected && !noneSelected)
+                        noneSelected = false;
+                    if (!l.IsSelected && allSelected)
+                        allSelected = false;
+                    if (!allSelected && !noneSelected)
+                        break;
+                }
+                if (allSelected)
+                    isSelected = true;
+                else if (noneSelected)
+                    isSelected = false;
+                else
+                    isSelected = null;
+                //Immer auslösen, weil im Parent dann SelectedItems neu ausgewertet werden muss.
+                OnChanged("IsSelected");
+            }
+        }
+
+        private Landschaftsgruppe landschaftsgruppe;
+        public Landschaftsgruppe Landschaftsgruppe
+        {
+            get { return landschaftsgruppe; }
+            set { Set(ref landschaftsgruppe, value); }
+        }
+
+        ExtendedObservableCollection<LandschaftItem> landschaften;
+        public ExtendedObservableCollection<LandschaftItem> Landschaften
+        {
+            get { return landschaften; }
+            set { Set(ref landschaften, value); }
+        }
+
+        private bool? isSelected = false;
+        public bool? IsSelected
+        {
+            get { return isSelected; }
+            set {
+                if(Set(ref isSelected, value) && value.HasValue)
+                {
+                    //disable event
+                    ignoreChangedEvent = true;
+                    foreach(var l in Landschaften)
+                        l.IsSelected = value.Value;
+                    //enable event
+                    ignoreChangedEvent = false;
+                }
+            }
+        }
+    }
+}
