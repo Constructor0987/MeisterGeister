526677ae28b8a02cb5cc7aeaba290b89a8200106
dschneider <dschneider@c5e1f230-26d4-41f2-a5c8-149495a0d85e>
2016-01-09 15:59:16 +0000

    Ergänzung BackgroundQueue
    git-svn-id: https://moonvega.pmhost.de/svn/meistergeister/trunk@2294 c5e1f230-26d4-41f2-a5c8-149495a0d85e

---
+++ Logic/General/BackgroundWorkerQueue.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeisterGeister.Logic.General
+{
+    public class BackgroundWorkerQueue : Queue<BackgroundWorkerQueueItem>
+    {
+        public event RunWorkerCompletedEventHandler RunWorkerCompleted;
+        public event ProgressChangedEventHandler ProgressChanged;
+
+        public void QueueWorker(BackgroundWorkerQueueItem worker, string userState)
+        {
+            worker.WorkerReportsProgress = true;
+            worker.WorkerSupportsCancellation = true;
+
+            worker.DoWork += (s, a) =>
+            {
+                worker.ReportProgress(0, userState);
+            };
+
+            worker.ProgressChanged += (s, a) =>
+            {
+                if (ProgressChanged != null)
+                    ProgressChanged(s, a);
+            };
+
+            worker.RunWorkerCompleted += (s, a) =>
+            {
+                Dequeue();
+                if (RunWorkerCompleted != null)
+                    RunWorkerCompleted(s, a);
+
+                if (Count > 0)
+                {
+                    var next = Peek();
+                    next.RunWorkerAsync(next.Sender);
+                }
+                else if (ProgressChanged != null)
+                    ProgressChanged(s, new ProgressChangedEventArgs(100, "Vorgang abgeschlossen"));
+            };
+
+            Enqueue(worker);
+            if (Count == 1)
+            {
+                var next = Peek();
+                next.RunWorkerAsync(next.Sender);
+            }
+        }
+    }
+}
---
+++ Logic/General/BackgroundWorkerQueueItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeisterGeister.Logic.General
+{
+    public class BackgroundWorkerQueueItem : BackgroundWorker
+    {
+        public BackgroundWorkerQueueItem(object sender)
+        {
+            this.Sender = sender;
+        }
+
+        public object Sender { get; private set; }
+    }
+}
