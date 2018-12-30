7819374fb602c1802bbea5712f6aa4c005806689
Juergen Bos <juergenbos007@hotmail.de>
2018-12-30 21:38:38 +0100

    update time

--- View/General/MultipleColorSlider.xaml.cs
+++ View/General/MultipleColorSlider.xaml.cs
@@ -64,7 +64,17 @@ namespace MeisterGeister.View.General
         public double LowerValue
         {
             get { return (double)GetValue(LowerValueProperty); }
-            set { SetValue(LowerValueProperty, value); }
+            set
+            {
+                SetValue(LowerValueProperty, value);
+                LowerValueProcent = value / Maximum;
+            }
+        }
+
+        public double LowerValueProcent
+        {
+            get { return (double)GetValue(LowerValueProcentProperty); }
+            set { SetValue(LowerValueProcentProperty, value); }
         }
 
         public double Upper1Value
@@ -73,11 +83,25 @@ namespace MeisterGeister.View.General
             set { SetValue(Upper1ValueProperty, value); }
         }
 
+        public double Upper1ValueProcent
+        {
+            get { return (double)GetValue(Upper1ValueProcentProperty); }
+            set { SetValue(Upper1ValueProcentProperty, value); }
+        }
+
+
         public double Upper2Value
         {
             get { return (double)GetValue(Upper2ValueProperty); }
             set { SetValue(Upper2ValueProperty, value); }
         }
+
+        public double Upper2ValueProcent
+        {
+            get { return (double)GetValue(Upper2ValueProcentProperty); }
+            set { SetValue(Upper2ValueProcentProperty, value); }
+        }
+
 
         public double Maximum
         {
@@ -112,36 +136,42 @@ namespace MeisterGeister.View.General
         #endregion
 
         #region //---- REGISTER ----
-        public static readonly DependencyProperty ColorWechselProperty =
+        public static DependencyProperty ColorWechselProperty =
             DependencyProperty.Register("ColorWechsel", typeof(int), typeof(MultipleColorSlider), new UIPropertyMetadata(4));
 
-        public static readonly DependencyProperty MinimumProperty =
+        public static DependencyProperty MinimumProperty =
             DependencyProperty.Register("Minimum", typeof(double), typeof(MultipleColorSlider), new UIPropertyMetadata(0d));
-        public static readonly DependencyProperty LowerValueProperty =
+        public static DependencyProperty LowerValueProperty =
             DependencyProperty.Register("LowerValue", typeof(double), typeof(MultipleColorSlider), new UIPropertyMetadata(.1d, null, LowerValueCoerceValueCallback));
-        public static readonly DependencyProperty Upper1ValueProperty =
+        public static DependencyProperty LowerValueProcentProperty =
+            DependencyProperty.Register("LowerValueProcent", typeof(double), typeof(MultipleColorSlider), new UIPropertyMetadata(.1d));
+        public static DependencyProperty Upper1ValueProperty =
             DependencyProperty.Register("Upper1Value", typeof(double), typeof(MultipleColorSlider), new UIPropertyMetadata(.5d, null, Upper1ValueCoerceValueCallback));
-        public static readonly DependencyProperty Upper2ValueProperty =
+        public static DependencyProperty Upper1ValueProcentProperty =
+            DependencyProperty.Register("Upper1ValueProcent", typeof(double), typeof(MultipleColorSlider), new UIPropertyMetadata(.5d));
+        public static DependencyProperty Upper2ValueProperty =
             DependencyProperty.Register("Upper2Value", typeof(double), typeof(MultipleColorSlider), new UIPropertyMetadata(.9d, null, Upper2ValueCoerceValueCallback));
-        public static readonly DependencyProperty MaximumProperty =
+        public static DependencyProperty Upper2ValueProcentProperty =
+            DependencyProperty.Register("Upper2ValueProcent", typeof(double), typeof(MultipleColorSlider), new UIPropertyMetadata(.9d));
+        public static DependencyProperty MaximumProperty =
             DependencyProperty.Register("Maximum", typeof(double), typeof(MultipleColorSlider), new UIPropertyMetadata(1d));
-        public static readonly DependencyProperty IsSnapToTickEnabledProperty =
+        public static DependencyProperty IsSnapToTickEnabledProperty =
             DependencyProperty.Register("IsSnapToTickEnabled", typeof(bool), typeof(MultipleColorSlider), new UIPropertyMetadata(false));
-        public static readonly DependencyProperty TickFrequencyProperty =
+        public static DependencyProperty TickFrequencyProperty =
             DependencyProperty.Register("TickFrequency", typeof(double), typeof(MultipleColorSlider), new UIPropertyMetadata(0.1d));
-        public static readonly DependencyProperty TickPlacementProperty =
+        public static DependencyProperty TickPlacementProperty =
             DependencyProperty.Register("TickPlacement", typeof(TickPlacement), typeof(MultipleColorSlider), new UIPropertyMetadata(TickPlacement.BottomRight));
-        public static readonly DependencyProperty TicksProperty =
+        public static DependencyProperty TicksProperty =
             DependencyProperty.Register("Ticks", typeof(DoubleCollection), typeof(MultipleColorSlider), new UIPropertyMetadata(null));
-        public static readonly DependencyProperty StartColorProperty =
+        public static DependencyProperty StartColorProperty =
             DependencyProperty.Register("StartColor", typeof(Color), typeof(MultipleColorSlider), new UIPropertyMetadata(Colors.White));
-        public static readonly DependencyProperty EndColorProperty =
+        public static DependencyProperty EndColorProperty =
             DependencyProperty.Register("EndColor", typeof(Color), typeof(MultipleColorSlider), new UIPropertyMetadata(Colors.White));
-        public static readonly DependencyProperty FirstColorProperty =
+        public static DependencyProperty FirstColorProperty =
             DependencyProperty.Register("FirstColor", typeof(Color), typeof(MultipleColorSlider), new UIPropertyMetadata(Colors.White));
-        public static readonly DependencyProperty SecondColorProperty =
+        public static DependencyProperty SecondColorProperty =
             DependencyProperty.Register("SecondColor", typeof(Color), typeof(MultipleColorSlider), new UIPropertyMetadata(Colors.White));
-        public static readonly DependencyProperty ThirdColorProperty =
+        public static DependencyProperty ThirdColorProperty =
             DependencyProperty.Register("ThirdColor", typeof(Color), typeof(MultipleColorSlider), new UIPropertyMetadata(Colors.White));
 
         #endregion
@@ -157,10 +187,12 @@ namespace MeisterGeister.View.General
             MultipleColorSlider targetSlider = (MultipleColorSlider)target;
             double value = (double)valueObject;
 
-            return
-                (targetSlider.ColorWechsel >= 4) ?
-                Math.Min(value, targetSlider.Upper1Value - .01) :
+            double valReturn = (targetSlider.ColorWechsel >= 4) ?
+                Math.Min(value, targetSlider.Upper1Value * .99) :
                 value;
+            targetSlider.LowerValueProcent = valReturn / targetSlider.Maximum;
+            
+            return valReturn;
         }
 
         private static object Upper1ValueCoerceValueCallback(DependencyObject target, object valueObject)
@@ -168,10 +200,13 @@ namespace MeisterGeister.View.General
             MultipleColorSlider targetSlider = (MultipleColorSlider)target;
             double value = (double)valueObject;
 
-            return 
+            double valReturn =
                 (targetSlider.ColorWechsel >= 5)?
-                Math.Min(Math.Max(value, targetSlider.LowerValue + .01), targetSlider.Upper2Value - .01):
-                Math.Max(value, targetSlider.LowerValue + .01);
+                Math.Min(Math.Max(value, targetSlider.LowerValue * 1.01), targetSlider.Upper2Value * .99):
+                Math.Max(value, targetSlider.LowerValue * 1.01);
+            targetSlider.Upper1ValueProcent = valReturn / targetSlider.Maximum;
+
+            return valReturn;
 
         }
 
@@ -179,8 +214,10 @@ namespace MeisterGeister.View.General
         {
             MultipleColorSlider targetSlider = (MultipleColorSlider)target;
             double value = (double)valueObject;
+            double valReturn = Math.Max(value, targetSlider.Upper1Value * 1.01);
+            targetSlider.Upper2ValueProcent = valReturn / targetSlider.Maximum;
 
-            return Math.Max(value, targetSlider.Upper1Value + .01);
+            return valReturn;
         }
 
         #endregion
