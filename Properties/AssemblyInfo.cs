5ec2d623c5581209d8c204bf1f285976cd92bcb6
Juergen Bos <juergenbos007@hotmail.de>
2021-07-31 14:46:02 +0200

    Release auf 2.5.1.55

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -51,8 +51,8 @@ using System.Windows;
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.5.54.1")]
-[assembly: AssemblyFileVersion("2.5.54.1")]
+[assembly: AssemblyVersion("2.5.55.1")]
+[assembly: AssemblyFileVersion("2.5.55.1")]
 
 // Dem Testprojekt den Zugriff auf die Interna erlauben.
 [assembly: InternalsVisibleTo("MeisterGeister_Tests")]
